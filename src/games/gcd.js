2339bbe40f130b7b064d6bda353c520165dc1da9
ivanleonov <treska1315@gmail.com>
2021-12-01 14:45:35 +0300

    gcd upgraded

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -4,15 +4,8 @@ import getRandom from '../randomath.js';
 const gcd = () => {
   const rand1 = getRandom();
   const rand2 = getRandom();
-  const temp = [1];
-  let n = 1;
-  while (n <= rand1) {
-    if (rand1 % n === 0 && rand2 % n === 0) {
-      if (!temp.includes(n)) temp.push(n);
-      n += 1;
-    } else n += 1;
-  }
-  const res = temp[temp.length - 1];
+  const getGcd = (a, b) => ((a % b) ? getGcd(b, a % b) : Math.abs(b));
+  const res = getGcd(rand1, rand2);
   return res > 1 ? [`${rand1} ${rand2}`, res] : gcd();
 };
 
