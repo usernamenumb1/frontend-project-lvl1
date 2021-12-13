28e27140dac83f9edc2f451e863419cfa44fcb65
ivanleonov <treska1315@gmail.com>
2021-12-13 09:22:00 +0300

    cosmetical rechanges2

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 const isCorrect = (corrans, ans) => ((String(corrans) === ans) ? ans : !ans);
 
 const greeting = () => {
-  console.log('Welcome to the Brain Games');
+  console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   return name;
